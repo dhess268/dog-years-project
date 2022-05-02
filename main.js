be4244c9c9a11487a79abc54e6f39da88b22cdfb
Dillon Hess <83610287+dhess268@users.noreply.github.com>
2022-05-01 21:43:24 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// take in my age
+let myAge = 10
+
+// number of years which dog years increase the fastest
+let earlyYears = 2
+
+// convert years to dog years
+earlyYears *= 10.5
+
+// subtract early years from our age
+let laterYears = myAge - 2
+
+// convert later years to dog years
+laterYears *= 4
+
+myAgeInDogYears = earlyYears + laterYears
+
+myName = 'Dillon'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
